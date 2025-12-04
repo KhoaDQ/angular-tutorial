705a13bbd145777b4166c5ee2151212cf88a1cd9
kosey <quockhoathcstq@gmail.com>
2025-12-04 07:47:28 +0700

    binding data and navigating pages

--- hello-world/src/app/app.routes.ts
+++ hello-world/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home',
+    },
+    {   
+        path: 'home',
+        loadComponent: () => import('./home/home').then(m => m.Home)
+    },
+    {
+        path: 'todos',
+        loadComponent: () => import('./todos/todos').then(m => m.Todos)
+    }
+];
--- hello-world/src/app/home/home.ts
+++ hello-world/src/app/home/home.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Greeting } from "../components/greeting/greeting";
+import { Counter } from "../components/counter/counter";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Greeting, Counter],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+  message = signal('Welcome to the Home Page!');
 
+  keyUpHandler(event: KeyboardEvent) {
+    console.log(`Key Up Event: ${event.key} key`);
+  }
 }
